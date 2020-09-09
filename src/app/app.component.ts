279d471817260939556b96e5c374e52e829d0f6a
Manali Sahu <manali6421@gmail.com>
2020-09-09 10:58:04 -1200

    task done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'Task-boxes';
+
+  top;
+  value = {};
+
+  clickListener(id): void {
+    if (id in this.value && !this.value[id]) {
+      this.top = document.getElementById(id);
+      this.top.style['z-index'] = 2;
+      this.value[id] = true;
+    } else if (id in this.value && this.value[id]) {
+      this.top = document.getElementById(id);
+      this.top.style['z-index'] = -2;
+      this.value[id] = false;
+    } else {
+      this.top = document.getElementById(id);
+      this.top.style['z-index'] = 2;
+      this.value[id] = true;
+    }
+  }
 }
